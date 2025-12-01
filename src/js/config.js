f016ce4380b3826349e1568a8457a776e413a1d2
Sadegh Dehyadgari <sadeghdehyadgari@gmail.com>
2025-12-01 14:50:14 +0330

    pushing config.js to repo

---
+++ src/js/config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+  API: {
+    BASE_URL: "https://api.themoviedb.org/3/",
+    API_KEY: "51432160e87955a0adea34fbd691870f",
+    IMAGE_BASE_URL: "https://image.tmdb.org/t/p/",
+    TIMEOUT: 5000,
+  },
+  APP: {
+    ITEMS_PER_PAGE: 20,
+    DEBOUNCE_DELAY: 300,
+  },
+};
+
+export default CONFIG;
